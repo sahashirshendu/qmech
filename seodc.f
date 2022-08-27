8bd97839b40d5f7778637f142651466bb4521e21
Shirshendu Saha <shirshendusaha78@gmail.com>
2022-08-27 08:06:06 +0530

    Commit @ 2022-08-27 08:06:06

--- seodc.f
+++ seodc.f
@@ -33,13 +33,11 @@ c     gnuplot
       write(10,*) "set term post"
       write(10,*) "set out 'seod.ps'"
       write(10,*) "set nokey"
-      write(10,*) "set style data lines"
-      write(10,*) "set yrange [0.0:",d(ns)*1.20,"]"
-      write(10,*) "plot '-' u 1:(",d(1),"+$2), \"
+      write(10,*) "plot '-' u 1:(",d(1),"+$2) w l, \"
       do i = 2, ns - 1
-        write(10,*) "'-' u 1:(",d(i),"+$2), \"
+        write(10,*) "'-' u 1:(",d(i),"+$2) w l, \"
       end do
-      write(10,*) "'-' u 1:(",d(ns),"+$2)"
+      write(10,*) "'-' u 1:(",d(ns),"+$2) w l"
       do 17 i=1,ns
         do 18 j = 1, np
           write(10,*) j-1, a(j,i)
