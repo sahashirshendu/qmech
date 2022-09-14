5941a4b0a6dd56a70356637fafd9f103469bf4e9
Shirshendu Saha <shirshendusaha78@gmail.com>
2022-09-14 20:38:33 +0530

    Commit @ 2022-09-14 20:38:33

--- sgseq.f
+++ sgseq.f
@@ -1,52 +1,52 @@
       program sgseq
-        integer, allocatable :: comp(:, :)
-        integer atn, up, down, n, i, j
-        real rand
+      integer, allocatable :: comp(:, :)
+      integer atn, up, down, n, i, j
+      real rand
 
-        print *, "SGX - Press 1"
-        print *, "SGY - Press 2"
-        print *, "SGZ - Press 3"
-        print *,'Number of SG devices ='
-        read *, n
-        allocate(comp(n, 3))
-        do i = 1, n
-          print *, 'Choice of device =',i
-          read *, rand
-          comp(i, 1) = rand
-        enddo
-        print *, ''
-        print *, 'Number of atoms :'
-        read *, atn
+      print *, "SGX - Press 1"
+      print *, "SGY - Press 2"
+      print *, "SGZ - Press 3"
+      print *,'Number of SG devices ='
+      read *, n
+      allocate(comp(n, 3))
+      do i = 1, n
+        print *, 'Choice of device =',i
+        read *, rand
+        comp(i, 1) = rand
+      enddo
+      print *, ''
+      print *, 'Number of atoms :'
+      read *, atn
 
-        do i=1,n
-          if(i .eq. 1) then
+      do i=1,n
+        if(i .eq. 1) then
+          up = 0
+          do j=1,atn
+            call random_number(rand)
+            if (rand .gt. 0.5) then
+              up = up + 1
+            end if
+          end do
+          down = atn - up
+          comp(i, 2) = up
+          comp(i, 3) = down
+        else
+          atn = up
+          if(comp(i,1) .ne. comp(i-1,1)) then
             up = 0
-            do j=1,atn
+            do j = 1, atn
               call random_number(rand)
-              if (rand .gt. 0.5) then
+              if(rand .gt. 0.5) then
                 up = up + 1
               end if
             end do
-            down = atn - up
-            comp(i, 2) = up
-            comp(i, 3) = down
           else
-            atn = up
-            if(comp(i,1) .ne. comp(i-1,1)) then
-              up = 0
-              do j = 1, atn
-                call random_number(rand)
-                if(rand .gt. 0.5) then
-                  up = up + 1
-                end if
-              end do
-            else
-            endif
-            down = atn - up
-            comp(i, 2) = up
-            comp(i, 3) = down
           endif
+          down = atn - up
+          comp(i, 2) = up
+          comp(i, 3) = down
+        endif
 
-          print *, i, (comp(i,j),j=1,3)
-        enddo
+        print *, i, (comp(i,j),j=1,3)
+      enddo
       end
