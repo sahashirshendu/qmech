9d3d42b7dae9283991c997614f588bce70a144df
Shirshendu Saha <shirshendusaha78@gmail.com>
2022-11-25 14:45:56 +0530

    Commit @ 2022-11-25 14:45:56

--- hon.f
+++ hon.f
@@ -1,25 +1,25 @@
       program numerov
       integer, parameter :: n = 100
-      real, dimension(0:n) :: x, wfn
+      real, dimension(n) :: x, wfn
       real en, emin, emax, energy, enval, k2, dx, xmin, L
       common x, wfn, en, dx
       external enval
 
-      open(1,file="num.txt")
+      open(1,file='hon.txt')
       L = 8
       xmin = - 0.5 * L
       emin = 0.0
       emax = 1.0
 
-      dx = L / n
-      do i = 0, n
-         x(i) = xmin + i * dx
+      dx = L / (n - 1)
+      do i = 1, n
+         x(i) = xmin + (i - 1) * dx
       end do
 
       call bisection(enval, emin, emax, energy)
       write(*,*) 'Energy =', energy
 
-      do i = 0, n
+      do i = 1, n
       write (1, *) x(i), wfn(i)
       end do
       stop
@@ -27,16 +27,16 @@
 
       function enval(energy)
       integer, parameter :: n = 100
-      real, dimension(0:n) :: x, wfn, k2
+      real, dimension(n) :: x, wfn, k2
       common x, wfn, en, dx
 
       en = energy
-      do i = 0, n
+      do i = 1, n
         k2(i) = 2.0 * (en - 0.5 * x(i) ** 2)
       end do
-      wfn(0) = 0.0
-      wfn(1) = 1.0e-5
-      do i = 1, n - 1, 1
+      wfn(1) = 0.0
+      wfn(2) = 1.0e-5
+      do i = 2, n - 1, 1
         cm = 1.0 + dx ** 2 / 12.0 * k2(i - 1)
         cz = 2.0 * (1.0 - 5.0 / 12.0 * dx ** 2 * k2(i))
         cp = 1.0 + dx ** 2 / 12.0 * k2(i + 1)
