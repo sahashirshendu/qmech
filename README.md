00d470897bd1af45f83163ca97c95aec112e9b43
Shirshendu Saha <shirshendusaha78@gmail.com>
2022-11-25 14:43:00 +0530

    Commit @ 2022-11-25 14:43:00

--- README.md
+++ README.md
@@ -38,6 +38,5 @@ Quantum Mechanics Lab Code
   ```gnuplot
   set term post color
   set out 'hon.ps'
-  set yrange [0:2]
-  plot 'hon.txt' u 1:2 w l title 'Ground State', 'hon.txt' u 1:3 w l title 'Potential'
+  plot 'hon.txt' u 1:2 w l
   ```
---
+++ hon.f
@@ -0,0 +1,61 @@
+      program numerov
+      integer, parameter :: n = 100
+      real, dimension(0:n) :: x, wfn
+      real en, emin, emax, energy, enval, k2, dx, xmin, L
+      common x, wfn, en, dx
+      external enval
+
+      open(1,file="num.txt")
+      L = 8
+      xmin = - 0.5 * L
+      emin = 0.0
+      emax = 1.0
+
+      dx = L / n
+      do i = 0, n
+         x(i) = xmin + i * dx
+      end do
+
+      call bisection(enval, emin, emax, energy)
+      write(*,*) 'Energy =', energy
+
+      do i = 0, n
+      write (1, *) x(i), wfn(i)
+      end do
+      stop
+      end
+
+      function enval(energy)
+      integer, parameter :: n = 100
+      real, dimension(0:n) :: x, wfn, k2
+      common x, wfn, en, dx
+
+      en = energy
+      do i = 0, n
+        k2(i) = 2.0 * (en - 0.5 * x(i) ** 2)
+      end do
+      wfn(0) = 0.0
+      wfn(1) = 1.0e-5
+      do i = 1, n - 1, 1
+        cm = 1.0 + dx ** 2 / 12.0 * k2(i - 1)
+        cz = 2.0 * (1.0 - 5.0 / 12.0 * dx ** 2 * k2(i))
+        cp = 1.0 + dx ** 2 / 12.0 * k2(i + 1)
+        wfn(i + 1) = (cz * wfn(i) - cm * wfn(i - 1)) / cp
+      end do
+      enval = wfn(n)
+      return
+      end
+
+      subroutine bisection(f, a, b, x)
+      do 11 i = 1, 100
+      x = (a + b) / 2
+      if ((f(x) * f(b)) .le. 0.0) then
+      a = x
+      else
+      b = x
+      end if
+      err = abs((b - a) / b)
+      if (err .lt. 0.000001) exit
+11    continue
+      return
+      end
